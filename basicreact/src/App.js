b437a9988d72af2a70b62863bb2cc9294713cbaf
SinhaAshi <sinhaashi39@gmail.com>
2025-08-05 11:22:08 +0530

    Learnt about component, prop and more

---
+++ basicreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chai from './Chai';
+
+function App() {
+  return (
+    <Chai />
+  );
+}
+
+export default App;
+//Always keep the component name in uppercase.
+//if the component is returning a HTML element then name it as .jsx 
---
+++ basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+function Chai() {
+  return (
+    <div>
+      <h1>Chai Component</h1>
+      <p>This is a simple component that displays a message.</p>
+    </div>
+  );
+}
+export default Chai;
